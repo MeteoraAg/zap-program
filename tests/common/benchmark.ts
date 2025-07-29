04af94fc00a7fc00528c5596b738fad46c99b779
Tu Phan <tu@racoons.dev>
2025-07-29 13:37:27 +0700

    feat: support distribution cu chart

--- tests/common/benchmark.ts
+++ tests/common/benchmark.ts
@@ -1,10 +1,16 @@
 import { BN } from "@coral-xyz/anchor";
+import babar from "babar";
 
 export class CuBenchmark {
   public numberOfTestcases = new BN(0);
   public totalCU = new BN(0);
   private worstCase: BN;
   private bestCase: BN;
+
+  private step = 10000;
+  private distribution: Array<[number, number]> = [...Array(140).keys()].map(
+    (i) => [i * this.step, 0]
+  );
 
   constructor(
     public readonly regex = /consumed (\d+) of (\d+) compute units/
@@ -17,9 +23,14 @@ export class CuBenchmark {
       if (!match) continue;
       this.numberOfTestcases = this.numberOfTestcases.add(new BN(1));
       const cu = new BN(match[1]);
+      // Basics
       this.totalCU = this.totalCU.add(cu);
       this.bestCase = !this.bestCase ? cu : BN.min(this.bestCase, cu);
       this.worstCase = !this.worstCase ? cu : BN.max(this.worstCase, cu);
+      // Distribution
+      const step = 10000;
+      const x = cu.div(new BN(step)).toNumber();
+      this.distribution[x][1] += 1;
       break;
     }
   };
@@ -43,5 +54,7 @@ export class CuBenchmark {
     console.log(`\r Avg. CU: ${this.avg().toString()}`);
     console.log(`\r Best CU: ${this.best().toString()}`);
     console.log(`\r Worst CU: ${this.worst().toString()}`);
+    console.log(`\r CU Distribution:`);
+    console.log(`\r ${babar(this.distribution)}`);
   };
 }
