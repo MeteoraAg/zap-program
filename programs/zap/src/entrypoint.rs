18e0fa7268638f8197908669c7d7cbda03c23842
Tu <tu@raccoons.dev>
2025-09-05 14:02:23 +0700

    feat: branching entrypoint

--- programs/zap/src/entrypoint.rs
+++ programs/zap/src/entrypoint.rs
@@ -1,103 +1,37 @@
-use std::{cell::RefCell, collections::BTreeSet, rc::Rc, u64};
-
-use anchor_lang::{
-    context::Context,
-    solana_program::{account_info::AccountInfo, msg, pubkey::Pubkey},
-    Accounts, AnchorDeserialize, Discriminator,
-};
+use anchor_lang::solana_program::entrypoint as a_entrypoint;
 use pinocchio::{
-    account_info::AccountInfo as pAccountInfo, entrypoint, program_error::ProgramError,
-    pubkey::Pubkey as pPubkey, ProgramResult,
+    account_info::AccountInfo as pAccountInfo, entrypoint as p_entrypoint, MAX_TX_ACCOUNTS, SUCCESS,
 };
-use pinocchio_log::log;
 
-use crate::{instruction, zap, ZapOutCtx, ZapOutCtxBumps, ZapOutParameters};
+use crate::entry;
 
-pub struct BridgedAccountInfo<'a> {
-    key: Pubkey,
-    lamports: &'a mut u64,
-    data: &'a mut [u8],
-    owner: Pubkey,
-    is_signer: bool,
-    is_writable: bool,
-    executable: bool,
-}
+#[no_mangle]
+pub unsafe extern "C" fn entrypoint(input: *mut u8) -> u64 {
+    if false {
+        const UNINIT: core::mem::MaybeUninit<pAccountInfo> =
+            core::mem::MaybeUninit::<pAccountInfo>::uninit();
+        // Create an array of uninitialized account infos.
+        let mut accounts = [UNINIT; MAX_TX_ACCOUNTS];
+        let (program_id, count, instruction_data) =
+            p_entrypoint::deserialize::<MAX_TX_ACCOUNTS>(input, &mut accounts);
 
-impl<'a> From<&'a mut BridgedAccountInfo<'a>> for AccountInfo<'a> {
-    fn from(info: &'a mut BridgedAccountInfo<'a>) -> Self {
-        AccountInfo {
-            key: &info.key,
-            lamports: Rc::new(RefCell::new(info.lamports)),
-            data: Rc::new(RefCell::new(info.data)),
-            owner: &info.owner,
-            rent_epoch: u64::MAX,
-            is_signer: info.is_signer,
-            is_writable: info.is_writable,
-            executable: info.executable,
+        // Call the program's entrypoint passing `count` account infos; we know that
+        // they are initialized so we cast the pointer to a slice of `[AccountInfo]`.
+        // match $process_instruction(
+        //     &program_id,
+        //     core::slice::from_raw_parts(accounts.as_ptr() as _, count),
+        //     &instruction_data,
+        // ) {
+        //     Ok(()) => $crate::SUCCESS,
+        //     Err(error) => error.into(),
+        // }
+
+        SUCCESS
+    } else {
+        let (program_id, accounts, instruction_data) = unsafe { a_entrypoint::deserialize(input) };
+        match entry(program_id, &accounts, instruction_data) {
+            Ok(()) => a_entrypoint::SUCCESS,
+            Err(error) => error.into(),
         }
     }
-}
-
-entrypoint!(process_instruction);
-
-pub unsafe fn process_instruction(
-    program_id: &pPubkey,
-    accounts: &[pAccountInfo],
-    data: &[u8],
-) -> ProgramResult {
-    let (discriminator, mut instruction_data) = data.split_at(8);
-
-    match discriminator {
-        data if data == instruction::ZapOut::DISCRIMINATOR => {
-            log!("Zap Out");
-
-            let program_id = Pubkey::new_from_array(*program_id);
-            let mut bumps = ZapOutCtxBumps::default();
-
-            let accounts_iter = &mut accounts.iter();
-
-            let mut bridged_account_info: Vec<BridgedAccountInfo> = accounts_iter
-                .map(|info| BridgedAccountInfo {
-                    key: Pubkey::new_from_array(*info.key()),
-                    lamports: info.borrow_mut_lamports_unchecked(),
-                    data: info.borrow_mut_data_unchecked(),
-                    owner: Pubkey::new_from_array(*info.owner()),
-                    is_signer: info.is_signer(),
-                    is_writable: info.is_writable(),
-                    executable: info.executable(),
-                })
-                .collect();
-            let account_info: Vec<AccountInfo> =
-                bridged_account_info.iter_mut().map(|e| e.into()).collect();
-
-            let mut zap_out_ctx = ZapOutCtx::try_accounts(
-                &program_id,
-                &mut account_info.as_slice(),
-                instruction_data,
-                &mut bumps,
-                &mut BTreeSet::new(),
-            )
-            .map_err(|_| ProgramError::Custom(101))?;
-
-            let ctx = Context::new(
-                &program_id,
-                &mut zap_out_ctx,
-                account_info.as_slice(),
-                bumps,
-            );
-
-            let params = ZapOutParameters::deserialize(&mut instruction_data)
-                .map_err(|_| ProgramError::InvalidInstructionData)?;
-
-            zap::zap_out(ctx, params).map_err(|e| {
-                msg!("er {}", e);
-                ProgramError::Custom(100)
-            })?
-        }
-        _ => {
-            log!("default {}", discriminator);
-        }
-    }
-
-    Ok(())
 }
