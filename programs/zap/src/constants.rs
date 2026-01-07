88a0093f3587539a5ada0b35c01b967f3a51b8af
bangyro <229454856+bangyro@users.noreply.github.com>
2026-01-07 14:51:03 +0800

    feat: add damm v1 zap

--- programs/zap/src/constants.rs
+++ programs/zap/src/constants.rs
@@ -2,6 +2,12 @@ use anchor_lang::constant;
 use anchor_lang::{prelude::Pubkey, pubkey};
 
 #[constant]
+pub const DAMM_V1: Pubkey = pubkey!("Eo7WjKq67rjJQSZxS6z3YkapzY3eMj6Xy8X5EQVn5UaB");
+// https://github.com/MeteoraAg/zap-program/blob/main/idls/damm_v2.json#L3512-L3521
+#[constant]
+pub const DAMM_V1_SWAP_DISC: [u8; 8] = [248, 198, 158, 145, 225, 117, 135, 200];
+
+#[constant]
 pub const DAMM_V2: Pubkey = pubkey!("cpamdpZCGKUy5JxQXB4dcpGPiikHawvSWAd6mEn1sGG");
 // https://github.com/MeteoraAg/zap-program/blob/main/idls/damm_v2.json#L3512-L3521
 #[constant]
@@ -22,7 +28,8 @@ pub const DLMM: Pubkey = pubkey!("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo");
 pub const DLMM_SWAP2_DISC: [u8; 8] = [65, 75, 63, 76, 235, 91, 91, 136];
 
 #[constant]
-pub const WHITELISTED_AMM_PROGRAMS: [(Pubkey, [u8; 8]); 4] = [
+pub const WHITELISTED_AMM_PROGRAMS: [(Pubkey, [u8; 8]); 5] = [
+    (DAMM_V1, DAMM_V1_SWAP_DISC),
     (DAMM_V2, DAMM_V2_SWAP_DISC),
     (DLMM, DLMM_SWAP2_DISC),
     (JUP_V6, JUP_V6_ROUTE_DISC),
