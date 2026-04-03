713b907ffba8e87b8feb8c48a4c116e584f36d12
bangyro <229454856+bangyro@users.noreply.github.com>
2026-04-03 17:57:02 +0800

    feat: add dlmm test address

--- zap-sdk/src/constants.rs
+++ zap-sdk/src/constants.rs
@@ -6,6 +6,9 @@ pub const JUP_V6_ROUTE_DISC: [u8; 8] = [229, 23, 203, 151, 122, 227, 173, 42];
 // https://github.com/MeteoraAg/zap-program/blob/4b67bfc64e5a023a1b74386be8b82c3908934a0b/idls/jupiter.json#L270
 pub const JUP_V6_SHARED_ACCOUNT_ROUTE_DISC: [u8; 8] = [193, 32, 155, 51, 65, 214, 156, 129];
 
+#[cfg(feature = "localnet")]
+pub const DLMM: Pubkey = Pubkey::from_str_const("LbVRzDTvBDEcrthxfZ4RL6yiq3uZw8bS6MwtdY6UhFQ");
+#[cfg(not(feature = "localnet"))]
 pub const DLMM: Pubkey = Pubkey::from_str_const("LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo");
 // https://github.com/MeteoraAg/zap-program/blob/4b67bfc64e5a023a1b74386be8b82c3908934a0b/idls/dlmm.json#L5242-L5251
 pub const DLMM_SWAP2_DISC: [u8; 8] = [65, 75, 63, 76, 235, 91, 91, 136];
